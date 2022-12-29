c0ee20be794f42f935c918244e1d261fe361278e
Iago Pires <iagopires.dev@gmail.com>
2022-12-29 15:59:57 -0400

    added all the files

---
+++ javascript/letConst.js
@@ -0,0 +1,12 @@
+let nome = 'Iago Santiago Pires';
+const idade = 21;
+let peso = 69.4;
+let altura = 1.73;
+
+let imc = peso / (altura * altura);
+
+console.log(nome, 'tem', idade, 'anos, pesa', peso, 'kg', 'tem', altura, 'de altura e seu IMC é de', imc);
+console.log(`${nome} tem ${idade} anos, pesa ${peso}kg, tem ${altura} de altura e seu IMC e de ${imc}`);
+console.log(nome + ' tem ' + idade + ' anos, pesa '+ peso + 'kg, tem ' + altura + ' de altura e seu IMC e de ' + imc);
+
+parseInt
