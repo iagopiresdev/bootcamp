8b9f4d72a23f2ff634fb4e503a16eeb61a6b837c
Iago Pires <iagopires.dev@gmail.com>
2023-01-08 15:40:50 -0400

    changed a few things

---
+++ fullstack/bootcamp/javascript/basics.js
@@ -0,0 +1,41 @@
+//create a documentation for all the basic functions of javascript
+
+//A variable that can be changed later on (causing problems)
+var idadeVar = 18;
+
+//A variable that can be changed later on
+let idadeLet = 18;
+
+//A variable that can't be changed later on
+const idadeConst = 18;
+
+//string
+let nome = 'Iago Santiago Pires';
+
+//list
+let hobbies = ['cozinhar', 'jogar'];
+
+//object
+let person = {
+    name: 'Max', //Property (blue box)
+    greet(){ //Method (purple box)
+        console.log('Hello!');
+    }
+};
+
+//to acess the method:
+person.greet(/*parameters*/); //methods are functions that are inside of objects, called using the . notation
+
+//function
+function calcularNascimento(/*parameters*/){
+    return idadeConst - 2023;
+}
+
+//arrow function
+const calcularNascimento = (/*parameters*/) => {return idadeConst - 2023;}
+
+//arrow function with only one parameter and one line of code
+const calcularNascimento = idadeConst => idadeConst - 2023;
+
+//to call the function:
+calcularNascimento(/*parameters*/);
